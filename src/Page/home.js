fe8b5a1655e3f8f709add2a6f341d0c788466e38
Nur-NobyRahaman <github943@gmail.com>
2023-02-05 10:58:22 +0600

    first commit

---
+++ src/Page/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../Component/Home/Home';
+
+const home = () => {
+    return (
+        <div>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default home;
